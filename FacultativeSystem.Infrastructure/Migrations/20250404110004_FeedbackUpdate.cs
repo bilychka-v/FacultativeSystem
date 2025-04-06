c381e4af84bcf5b9cb5cfb9f6cf7e58699c7749d
VikaBilyk <128689260+VikaBilyk@users.noreply.github.com>
2025-04-06 17:35:38 +0300

    updated migrations

---
+++ FacultativeSystem.Infrastructure/Migrations/20250404110004_FeedbackUpdate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FacultativeSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FeedbackUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_FeedbackGradeEntities_CourseId",
+                table: "FeedbackGradeEntities",
+                column: "CourseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FeedbackGradeEntities_Courses_CourseId",
+                table: "FeedbackGradeEntities",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FeedbackGradeEntities_Courses_CourseId",
+                table: "FeedbackGradeEntities");
+
+            migrationBuilder.DropIndex(
+                name: "IX_FeedbackGradeEntities_CourseId",
+                table: "FeedbackGradeEntities");
+        }
+    }
+}
---
+++ FacultativeSystem.Infrastructure/Migrations/20250404113147_FeedbackUpdateStudents.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FacultativeSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FeedbackUpdateStudents : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FeedbackGradeEntities_Students_StudentEntityId",
+                table: "FeedbackGradeEntities");
+
+            migrationBuilder.DropIndex(
+                name: "IX_FeedbackGradeEntities_StudentEntityId",
+                table: "FeedbackGradeEntities");
+
+            migrationBuilder.DropColumn(
+                name: "StudentEntityId",
+                table: "FeedbackGradeEntities");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FeedbackGradeEntities_StudentId",
+                table: "FeedbackGradeEntities",
+                column: "StudentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FeedbackGradeEntities_Students_StudentId",
+                table: "FeedbackGradeEntities",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FeedbackGradeEntities_Students_StudentId",
+                table: "FeedbackGradeEntities");
+
+            migrationBuilder.DropIndex(
+                name: "IX_FeedbackGradeEntities_StudentId",
+                table: "FeedbackGradeEntities");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "StudentEntityId",
+                table: "FeedbackGradeEntities",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FeedbackGradeEntities_StudentEntityId",
+                table: "FeedbackGradeEntities",
+                column: "StudentEntityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FeedbackGradeEntities_Students_StudentEntityId",
+                table: "FeedbackGradeEntities",
+                column: "StudentEntityId",
+                principalTable: "Students",
+                principalColumn: "Id");
+        }
+    }
+}
