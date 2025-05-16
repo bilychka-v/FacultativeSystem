b2c6f5d3493a85e959d309af0a571cd9b627c62b
VikaBilyk <128689260+VikaBilyk@users.noreply.github.com>
2025-05-17 01:04:43 +0300

    updated migrations

---
+++ FacultativeSystem.Infrastructure/Migrations/20250412153715_UpdateCourseEntities.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FacultativeSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCourseEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasUnmarkedStudents",
+                table: "Courses",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasUnmarkedStudents",
+                table: "Courses");
+        }
+    }
+}
